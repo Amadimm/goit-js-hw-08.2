1bd9f317420200b15e465b3cc6459721a146c55c
Amadimm <amadeuszrolecki@gmail.com>
2023-05-06 18:25:38 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const throttle = require('lodash.throttle');
+const btnEl = document.querySelector('button[type = "submit"]');
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[name = "email"]');
+const messageEl = document.querySelector('textarea[name = "message"]');
 
+
+function updateForm() {
+  const updateItem = JSON.parse(
+    localStorage.getItem(`feedback-form-state` || '')
+  );
+  emailEl.value = updateItem.email || '';
+  messageEl.value = updateItem.message || '';
+  
+}
+updateForm();
+
+function fillForm() {
+  const data = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+  
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+
+function clearForm(e) {
+  e.preventDefault();
+  console.log(data);
+  localStorage.clear();
+  formEl.reset();
+  
+}
+
+formEl.addEventListener('input', throttle(fillForm, 500));
+btnEl.addEventListener('submit', clearForm);
