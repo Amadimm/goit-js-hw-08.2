ba8676c9bf6e8f07e57089168c1a834bd7b75492
Amadimm <amadeuszrolecki@gmail.com>
2023-05-06 16:07:25 +0200

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoEL = document.querySelector('#vimeo-player');
+const player = new Player(vimeoEL);
+
+
+const play = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', play);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
