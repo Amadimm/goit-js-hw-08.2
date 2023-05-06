8e1ddbacc6b53b6193fb51f05d53f3c521533839
Amadimm <amadeuszrolecki@gmail.com>
2023-05-06 15:10:39 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryList = document.querySelector(`.gallery`);
+
+const newImages = galleryItems.map(
+    image =>
+      `<li>
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+   
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(' ');
+
+galleryList.style.listStyleType = 'none';
+
+galleryList.insertAdjacentHTML('beforeend', newImages);
+
 console.log(galleryItems);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
